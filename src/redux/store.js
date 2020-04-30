3b93fe37ffc6f2e3a5312533281b0d2aca0ccf86
Nicholas Roberto <njr444@gmail.com>
2020-04-30 10:39:59 -0700

    updated our application app component, sign-in component, sign-up component to use hooks

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,21 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import createSageMiddleware from 'redux-saga';
+import { fetchCollectionsStart } from '../redux/shop/shop.sagas';
+import rootSaga from '../redux/root-saga';
 
-const middlewares = [];
+const sagaMiddleware = createSageMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
